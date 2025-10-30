cf5c47498024e2e9d66eafb34ff7eb9f0bdc8ac1
simo hamzaoui <simo.hamzaoui@gmail.com>
2025-10-30 11:53:04 +0100

    prices done

--- resources/js/Components/PricingSection.vue
+++ resources/js/Components/PricingSection.vue
@@ -1,74 +1,69 @@
 <template>
 
-      <section id="pricing" class="pricing-section pricing-style-4 bg-light">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-xl-6 col-lg-6">
-            <div class="section-title mb-60">
-              <h3 class="mb-15 wow fadeInUp" data-wow-delay=".2s">Preise</h3>
-            </div>
-            <div class="price-image text-lg-right wow fadeInUp" data-wow-delay=".5s">
-              <img src="img/prices/price-img.svg" alt="">
-            </div>
+  <section id="pricing" class="pricing-section pricing-style-4 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-8 pricing-img">
+          <div class="section-title mb-60">
+            <h3 class="mb-15 wow fadeInUp" data-wow-delay=".2s">Preise</h3>
           </div>
-          <div class="col-xl-6 col-lg-6">
-            <div class="pricing-active-wrapper wow fadeInUp" data-wow-delay=".4s">
-              <div class="pricing-active">
-                <!-- Plan 1 -->
-                <div class="single-pricing-wrapper">
-                  <div class="single-pricing">
-                    <h6>Festpreis Starter</h6>
-                    <h4>Kleine Automatisierung</h4>
-                    <h3>ab 400 €</h3>
-                    <ul>
-                      <li><i class="fa-solid fa-check"></i> 1 Formular → PDF und E-Mail Versand</li>
-                      <li><i class="fa-solid fa-check"></i> Übernahme in Excel oder bestehendes CRM</li>
-                      <li><i class="fa-solid fa-check"></i> Kurze Einweisung und Übergabe</li>
-                      <li><i class="fa-solid fa-check"></i> Bereitstellung in wenigen Tagen</li>
-                    </ul>
-                    <a href="#contact" class="button radius-30">Anfragen</a>
-                  </div>
-                </div>
+          <div class="price-image text-lg-right wow fadeInUp" data-wow-delay=".5s">
+            <img src="img/prices/price-img.svg" alt="">
+          </div>
+        </div>
+        <div class="col-12 ">
+          <div class="pricing-active pricing-items">
+            <!-- Plan 1 -->
+            <div class="single-pricing-wrapper">
+              <div class="single-pricing">
+                <h6>Festpreis Starter</h6>
+                <h4>Kleine Automatisierung</h4>
+                <h3>ab 400 €</h3>
+                <ul>
+                  <li><i class="fa-solid fa-check"></i> 1 Formular → PDF und E-Mail Versand</li>
+                  <li><i class="fa-solid fa-check"></i> Übernahme in Excel oder bestehendes CRM</li>
+                  <li><i class="fa-solid fa-check"></i> Kurze Einweisung und Übergabe</li>
+                  <li><i class="fa-solid fa-check"></i> Bereitstellung in wenigen Tagen</li>
+                </ul>
+                <a href="#contact" class="button radius-30">Anfragen</a>
+              </div>
+            </div>
 
-                <!-- Plan 2 -->
-                <div class="single-pricing-wrapper">
-                  <div class="single-pricing">
-                    <h6>Projekt Standard</h6>
-                    <h4>Workflow Automatisierung</h4>
-                    <h3>ab 950 €</h3>
-                    <ul>
-                      <li><i class="fa-solid fa-check"></i> 2–3 Schritte automatisiert (z. B. Angebot, Rechnung, Mail)</li>
-                      <li><i class="fa-solid fa-check"></i> Anbindung an Excel oder einfaches Dashboard</li>
-                      <li><i class="fa-solid fa-check"></i> 30 Minuten Schulung inklusive</li>
-                      <li><i class="fa-solid fa-check"></i> Klar definierte Abnahme</li>
-                    </ul>
-                    <a href="#contact" class="button radius-30">Kostenlose Einschätzung</a>
-                  </div>
-                </div>
+            <!-- Plan 2 -->
+            <div class="single-pricing-wrapper">
+              <div class="single-pricing">
+                <h6>Projekt Standard</h6>
+                <h4>Workflow Automatisierung</h4>
+                <h3>ab 950 €</h3>
+                <ul>
+                  <li><i class="fa-solid fa-check"></i> 2–3 Schritte automatisiert (z. B. Angebot, Rechnung, Mail)</li>
+                  <li><i class="fa-solid fa-check"></i> Anbindung an Excel oder einfaches Dashboard</li>
+                  <li><i class="fa-solid fa-check"></i> 30 Minuten Schulung inklusive</li>
+                  <li><i class="fa-solid fa-check"></i> Klar definierte Abnahme</li>
+                </ul>
+                <a href="#contact" class="button radius-30">Kostenlose Einschätzung</a>
+              </div>
+            </div>
 
-                <!-- Plan 3 -->
-                <div class="single-pricing-wrapper">
-                  <div class="single-pricing">
-                    <h6>Projekt Pro</h6>
-                    <h4>Individuelle Lösung</h4>
-                    <h3>ab 1.800 €</h3>
-                    <ul>
-                      <li><i class="fa-solid fa-check"></i> Mehrere Abläufe mit Schnittstellen</li>
-                      <li><i class="fa-solid fa-check"></i> Benutzerverwaltung oder Freigaben</li>
-                      <li><i class="fa-solid fa-check"></i> Tests, Dokumentation, Übergabe</li>
-                      <li><i class="fa-solid fa-check"></i> Optionale Erweiterungen nach Bedarf</li>
-                    </ul>
-                    <a href="#contact" class="button radius-30">Projekt besprechen</a>
-                  </div>
-                </div>
+            <!-- Plan 3 -->
+            <div class="single-pricing-wrapper">
+              <div class="single-pricing">
+                <h6>Projekt Pro</h6>
+                <h4>Individuelle Lösung</h4>
+                <h3>ab 1.800 €</h3>
+                <ul>
+                  <li><i class="fa-solid fa-check"></i> Mehrere Abläufe mit Schnittstellen</li>
+                  <li><i class="fa-solid fa-check"></i> Benutzerverwaltung oder Freigaben</li>
+                  <li><i class="fa-solid fa-check"></i> Tests, Dokumentation, Übergabe</li>
+                  <li><i class="fa-solid fa-check"></i> Optionale Erweiterungen nach Bedarf</li>
+                </ul>
+                <a href="#contact" class="button radius-30">Projekt besprechen</a>
               </div>
             </div>
           </div>
         </div>
-        <div class="row">
-
-        </div>
       </div>
-    </section>
+    </div>
+  </section>
 
 </template>
